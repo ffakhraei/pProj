88b3f2823edcc622bb945d04e235445bd72b5a1e
ffakhraei <ffakhrae@gmail.com>
2017-01-27 01:19:35 -0800

    stoneWall.py modified - still wron answer

--- python/codility/ch7/stoneWall.py
+++ python/codility/ch7/stoneWall.py
@@ -1,28 +1,54 @@
 #!/bin/env/python
 
-"""Solution is not correct"""
-
-
-
+H=[3,2,1]
 H=[8,8,5,7,9,8,7,4,8]
 
-def solution(H) :
-    minNum = 0
-    length = len(H)
-    minElem = min(H)
-    if length == 1 :
-        minNum += 1
-        return minNum
-    li.append(H[0])
-    for i in xrange(length-1) :
-        if H[i] == H[i+1] :
-            continue
+import functools
+
+global islandList, counter 
+islandList = []
+counter = 0
+def group(H) :
+    li = []
+    result = []
+    global counter
+    for i, item in enumerate(H) :
+        if item > 0 :
+            li.append(item)
         else :
-            if H[i+1] < li[0] :
-                li[0] = H[i+1] 
-                minNum += 1
+            if i < len(H) - 1 :
+                if H[i+1] == 0 :
+                    continue
+                else :
+                    counter += 1
             else :
-                minNum += 1
-    return minNum
+                counter += 1
+            if len(li) > 0 :
+                result.append(li)
+            li = []
+    if len(li) > 0 :
+        result.append(li)
+    return result
+
+def foo(a, b) : return a - b
+
+def crumble(myList) :
+    return map(functools.partial(foo, b=min(myList)), myList)
+
+def solution(H) :
+    global islandList
+    global counter
+    if len(H) == 0 :
+        return 0
+    crumbled = crumble(H)
+    islands = group(crumbled)
+    while len(islands) > 0 :
+        while len(islands) > 1 :
+            islandList.append(islands.pop())
+        crumbled = crumble(islands[0])
+        islands = group(crumbled)
+    if len(islandList) > 0 :
+        solution(islandList.pop())
+    return counter
 
 print solution(H)
