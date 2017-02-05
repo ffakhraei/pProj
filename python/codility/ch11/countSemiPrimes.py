4bfee855a0e87ed0a7649f953c267c6041648198
ffakhraei <ffakhrae@gmail.com>
2017-02-04 16:12:32 -0800

    initial commit - countSemiPrimes.py v0.0

---
+++ python/codility/ch11/countSemiPrimes.py
@@ -0,0 +1,16 @@
+#!/bin/env/python
+
+A=[4,6,9,10,14,15,21,22,25,26]
+P=[1,4,16]
+Q=[26,10,20]
+N=26
+
+from math import sqrt
+import itertools
+
+def solution(N,P,Q) :
+
+    return "I'm done"
+
+print solution(N,P,Q) 
+
