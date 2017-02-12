2cdccd383d7008696fb37491412e6a6850fd011e
ffakhraei <ffakhrae@gmail.com>
2017-02-12 02:50:47 -0800

    codility-ch13-q2 : refined solution to achive 83% correctness. BUG : produces wrong results in some cases; unable to come up with data to falsify the solution. Further testing required with new data set. closes #13

--- python/codility/ch13/fibFrog.py
+++ python/codility/ch13/fibFrog.py
@@ -1,24 +1,15 @@
 #/bin/env/python
 
-A=[1,1,0,0,0]
-#expected: 2
+A=[0,0,0,1,0]
+#expect -1
 A=[0,1,0,1,0]
 #expected: 3
 A=[]
 #expect 1
-A=[0,0,0,1,0]
-#expect -1
-A=[0,0,0,1,1,0,1,0,0,0,0]
-#expected results: 3
-
-#/bin/env/python
-
-A=[0,1,0,1,0]
-#expected: 3
-A=[0,0,0,1,1,0,1,0,0,0,0]
-#expected results: 3
 A=[1,1,0,0,0]
 #expected: 2
+A=[0,0,0,1,1,0,1,0,0,0,0]
+#expected results: 3
 
 def fibDynamic(n) :
     fib = [0] * (n+2)
@@ -27,31 +18,13 @@ def fibDynamic(n) :
         fib[i] = fib[i-1] + fib[i-2]
     return fib[n]
 
-def jump(A,i=0,numJumps=0) :
-    length = len(A)
-    if length <= 2 :
-        return 1
-    li = []
-    while fibDynamic(i) <= length+1 :
-        li.append(fibDynamic(i))
-        i += 1
-    li = set(li)
-    if length+1 in li :
-        return 1
-    i = length - 1
-    while i >= 0 :
-        if numJumps < 0 and i > 0 : 
-            i -= 1
-            return 
-            continue
-        if A[i] :
-            if i+1 in li :
-                numJumps += 1
-                return numJumps + solution(A[i+1:length])
-        i -= 1
-
+global crossed 
+global jumpIndexList
+crossed = False
+jumpIndexList = []
 
 def solution(A) :
+    global crossed
     length = len(A)
     if length <= 2 :
         return 1
@@ -60,22 +33,30 @@ def solution(A) :
     while fibDynamic(i) <= length+1 :
         li.append(fibDynamic(i))
         i += 1
-    del i
     li = set(li)
-    if length+1 in li :
-        return 1
-    i = length - 1
     numJumps = 0
-    while i >= 0 :
-        if numJumps < 0 and i > 0 : 
-            i -= 1
-            return 
-            continue
+    if length+1 in li : return numJumps + 1
+    i = length - 1
+    while i >= 0 and not crossed :
         if A[i] :
             if i+1 in li :
                 numJumps += 1
-                return numJumps + solution(A[i+1:length])
+                jumpIndexList.append(i)
+                if length - i in li : 
+                    numJumps += 1
+                    crossed = True
+                    return numJumps
+                numJumps += solution(A[i+1:length])
         i -= 1
-    return -1
+    if crossed :
+        return numJumps
+    if not crossed and len(jumpIndexList) == 0 :
+        return -1
+    else :
+        A[jumpIndexList.pop()] = 0
+        if len(jumpIndexList) > 0 :
+            return numJumps - 1 + solution(A[jumpIndexList[-1] + 1 : length])
+        else : 
+            return solution(A)
 
 print solution(A)
